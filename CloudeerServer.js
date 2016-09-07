6a39e660c5975ac748dac06a27bc9a512428a21c
Cloudbeer <cloudbeer@gmail.com>
2016-09-07 17:26:15 +0800

    登录命令已经完成

--- CloudeerServer.js
+++ CloudeerServer.js
@@ -8,26 +8,48 @@ function CloudeerServer(options) {
 }
 
 CloudeerServer.prototype.startService = function () {
+
   console.log("正在启动 Cloudeer 注册服务...");
+
   this.server = net.createServer((socket)=> {
     var _this = this;
     console.log('有客户端请求连接进入，等待身份认证...');
+
+    socket.on('data', (data)=> {
+      var jsonInfo;
+      try {
+        jsonInfo = JSON.parse(data.toString());
+      } catch (e) {
+        console.error("错误的数据，必须提供 json 格式的数据。");
+        console.log(data.toString());
+      }
+
+      console.log(jsonInfo);
+
+      if (!jsonInfo.cmd) {
+        console.error("错误的消息体，缺少 cmd 参数。");
+      } else {
+        switch (jsonInfo.cmd){
+          case "login":
+            _this.login(socket, jsonInfo.data.username, jsonInfo.data.password);
+            break;
+          case "reg-service":
+            break;
+        }
+
+        _this.arrangeServers();
+
+      }
+
+      // socket.tag = JSON.parse(data.toString());
+      // _this.clients.push(socket);
+
+    });
+
+
     socket.on('end', ()=> {
       console.log(socket.serviceName || "未命名", '微服务已经退出');
       _this.clients.splice(_this.clients.indexOf(socket), 1);
-    });
-
-    socket.on('data', (data)=> {
-      // var lala           = JSON.parse(data.toString());
-      // socket.serviceName = lala.name;
-      console.log(data.toString());
-      socket.tag = JSON.parse(data.toString());
-      _this.clients.push(socket);
-
-      console.log(_this.clients.length);
-      _this.clients.forEach(function (ele) {
-        console.log(ele.tag);
-      });
     });
   });
 
@@ -39,5 +61,26 @@ CloudeerServer.prototype.startService = function () {
     console.log('注册服务已经启动，监听端口：', this.port);
   });
 };
+
+CloudeerServer.prototype.arrangeServers = function () {
+
+  console.log(this.clients.length);
+  // this.clients.forEach(function (ele) {
+  //   //console.log(ele.);
+  // });
+};
+
+CloudeerServer.prototype.login = function (socket, username, password) {
+  if (username == "test" && password == "test") {
+    socket.write(JSON.stringify({errno: 0, cmd: 'login'}));
+    this.clients.push(socket);
+    console.log("有客户端加入，已经加入列队。");
+  } else {
+    console.error("登录错误，拒绝加入列队");
+    socket.end('{"errno": 403, "errText": "登录失败，拒绝连接。"}');
+    socket.destroy();
+  }
+};
+
 
 module.exports = CloudeerServer;
